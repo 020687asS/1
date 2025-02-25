eb5a2fd681b86292916391323184c475c22ae868
020687asawg <sofja.awg@gmail.com>
2025-02-25 12:08:26 +0300

    file add

---
+++ 24.7.2/tests/test_pet_friends.py
@@ -0,0 +1,66 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+import os
+
+
+pf = PetFriends()
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 200
+    assert 'key' in result
+
+
+def test_get_all_pets_with_valid_key(filter=''):
+    _,auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
+
+
+def test_add_new_pet(name = 'Гоша', animal_type = 'Волнистый попугай' , age = '12', pet_photo ='images/gosha.jpg'):
+    _,auth_key = pf.get_api_key(valid_email, valid_password)
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['name'] == name
+
+
+def test_add_new_photo_for_pet(pet_photo ='images/1.jpg'):
+    _,auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    pet_id = my_pets['pets'][0]['id']
+    if len(my_pets['pets']) > 0:
+        status, result = pf.add_pet_photo(auth_key, pet_id, pet_photo)
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+        assert status == 200
+        assert result['pet_photo'] == my_pets['pets'][0]['pet_photo']
+    else:
+        raise Exception('ops')
+
+
+def test_delete_self_pet():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_id = my_pets['pets'][0]['id']
+
+    status = pf.delete_pet_from_database(auth_key, pet_id)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    assert status == 200
+    assert pet_id not in my_pets.values()
+
+
+def test_update_information_about_pet(name = 'Гоша', animal_type = 'Волнистый попугай', age = '10'):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_id = my_pets['pets'][0]['id']
+    status, result = pf.update_information_about_pet(auth_key,pet_id, name, animal_type, age)
+    assert status == 200
+    assert result['age'] == age
+
+
+
+
+
